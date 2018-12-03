066144ab53f0a75eaf104b4ac8a273ab9a0f8737
Richard Evaristo <r.evaristo@coi.local>
2018-12-03 17:03:37 +0800

    created layout for login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,98 +1,66 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!DOCTYPE html>
+<html lang="en">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+</head>
+<body>
+    <div id="app">
+        <div id="login">
+            <div id="left">
+                <div id="showcase">
+                    <div class="showcase-content">
+                        <h1 class="showcase-text">
+                            Lorem, ipsum dolor. <strong>together</strong>
+                        </h1>
+                        <a href="#" class="btn secondary-outline-btn">Lorem, ipsum dolor.</a>
+                    </div>
                 </div>
             </div>
+            <div id="right">
+                <div class="signin">
+                    <div class="logo">
+                        <h1>BMS</h1>
+                    </div>
+                    <form>
+                        <div>
+                            <label>Username or Email</label>
+                            <input type="text" class="text-input">
+                        </div>
+                        <div>
+                            <label>Password</label>
+                            <input type="password" class="text-input">
+                        </div>
+                        <button type="button" class="btn primary-btn">Sign In</button>
+                    </form>
+                    <div class="links">
+                        <a href="#" class="btn secondary-btn">Forgot Password</a>
+                    </div>
+                    <div class="or">
+                        <hr class="bar">
+                        <span>OR</span>
+                        <hr class="bar">
+                    </div>
+                    <div class="social">
+                        <a href="#" class="btn primary-outline-btn">Sign in with <strong>Facebook</strong> <i class="fa fa-facebook-square"></i></a>
+                        <a href="#" class="btn danger-outline-btn">Sign in with <strong>Google</strong> <i class="fa fa-google-plus-square"></i></a>
+                        <a href="#" class="btn secondary-outline-btn">Create an account</a>
+                    </div>
+                </div>
+                <footer id="main-footer">
+                    <p>Copyright &copy; 2018, BMS All Rights Reserved</p>
+                    <div>
+                        <a href="#">terms of use</a> | <a href="#">Privacy Policy</a>
+                    </div>
+                </footer>
+            </div>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
