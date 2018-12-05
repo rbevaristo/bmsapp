468980885b512957ecf4aa75949525306de616b1
Richard Evaristo <r.evaristo@coi.local>
2018-12-05 14:18:23 +0800

    update dashboard layout

--- public/js/main.js
+++ public/js/main.js
@@ -4,12 +4,28 @@ sideMenuToggler.addEventListener('click', toggleSideMenu);
 
 function toggleSideMenu() {
     let aside = document.getElementById('sideMenu');
-    let headerNav = document.getElementById('headerNav');
-
-    if (aside.classList.contains('isOpen')) {
-        aside.classList.remove('isOpen');
-    } else {
+    (aside.classList.contains('isOpen')) ?
+    aside.classList.remove('isOpen'):
         aside.classList.add('isOpen');
-    }
-
 }
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('realTime').innerHTML =
+        h + ":" + m + ":" + s;
+    var t = setTimeout(startTime, 500);
+}
+
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i
+    }; // add zero in front of numbers < 10
+    return i;
+}
+
+startTime();
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,24 +16,32 @@
             <aside id="sideMenu">
                 <nav>
                     <a href="#" class="active">
-                        <i class="fa fa-home" aria-hidden="true"></i>
-                        <span> Home</span>
+                        <i class="fa fa-dashboard" aria-hidden="true"></i>
+                        <span> Overview</span>
                     </a>
                     <a href="#">
-                        <i class="fa fa-sticky-note" aria-hidden="true"></i>
-                        <span> Files</span>
-                    </a>
-                    <a href="#">
-                        <i class="fa fa-bookmark" aria-hidden="true"></i>
-                        <span> Bookmark</span>
+                        <i class="fa fa-tasks" aria-hidden="true"></i>
+                        <span> Projects</span>
                     </a>
                     <a href="#">
                         <i class="fa fa-calendar" aria-hidden="true"></i>
-                        <span> Calendar</span>
+                        <span> Schedules</span>
                     </a>
                     <a href="#">
-                        <i class="fa fa-user-circle" aria-hidden="true"></i>
-                        <span> Users</span>
+                        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
+                        <span> Sales</span>
+                    </a>
+                    <a href="#">
+                        <i class="fa fa-file" aria-hidden="true"></i>
+                        <span> Reports</span>
+                    </a>
+                    <a href="#">
+                        <i class="fa fa-users" aria-hidden="true"></i>
+                        <span> Employees</span>
+                    </a>
+                    <a href="#">
+                        <i class="fa fa-money" aria-hidden="true"></i>
+                        <span> Payroll</span>
                     </a>
                     <a href="#">
                         <i class="fa fa-cog" aria-hidden="true"></i>
@@ -42,16 +50,11 @@
                 </nav>
             </aside>
             <header id="headerNav">
-                <ul>
-                    <li>
-                        <button type="button" class="btn sm-btn" id="sideMenu--toggler">
-                            <i class="fa fa-bars"></i>
-                        </button>
-                    </li>
-                    <li>
-                        <h1>BMS</h1>
-                    </li>
-                </ul>
+                <button type="button" class="btn sm-btn" id="sideMenu--toggler">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <h1>BMS</h1>
+
                 {{-- <div class="row">
                     <div class="col-md-12 col-sm-12">
 
@@ -73,6 +76,52 @@
                     </div>
                 </div> --}}
             </header>
+            <main id="content-area">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12">
+                        <div class="heading">
+                            <h1>Dashboard</h1>
+                            <p>Welcome <strong>Richard</strong></p>
+                        </div>
+                        <div class="dateNtime">
+                            <span>{{ date('F d, Y') }}</span>
+                            <span id="realTime"> </span>
+                        </div>
+                    </div>
+                </div>
+                <div class="container">
+                    <div class="col-md-5 col-sm-12">
+                        <div class="card">
+                            <div class="icon-lg">
+                                <i class="fa fa-tasks"></i>
+                            </div>
+                            <div class="icon-date-title">
+                                <i class="fa fa-calendar icon-sm"></i>
+                                <span>December</span>
+                                <h1>Projects</h1>
+                            </div>
+                            <div class="icon-count">
+                                <span>5</span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-5 col-sm-12">
+                        <div class="card">
+                            <div class="icon-lg">
+                                <i class="fa fa-shopping-cart"></i>
+                            </div>
+                            <div class="icon-date-title">
+                                <i class="fa fa-calendar icon-sm"></i>
+                                <span>December</span>
+                                <h1>Sales</h1>
+                            </div>
+                            <div class="icon-count">
+                                <span>100,000</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
     </div>
 
